285e0b759ee42a669e43902d7867885f12b2dbc1
Your Name <nickyoungdo@naver.com>
2021-08-07 22:09:02 +0900

    prac

---
+++ 4주차/prac/app.py
@@ -0,0 +1,39 @@
+from flask import Flask,render_template,request,jsonify
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+   return render_template('index.html')
+ #render template가 index.html을 가져와서 client(나)에게 줌
+
+@app.route('/test', methods=['GET'])
+def test_get():
+   title_receive = request.args.get('title_give')#title_give로 가져온 값을 가져와 '봅날은 간다'
+   print(title_receive)
+   return jsonify({'result':'success', 'msg': '이 요청은 GET!'})
+# 요청 확인 ajax코드
+# $.ajax({
+#     type: "GET",
+#     url: "/test?title_give=봄날은간다",
+#     data: {},
+#     success: function(response){
+#        console.log(response)
+#     }
+# })
+@app.route('/test', methods=['POST'])
+def test_post():
+   title_receive = request.form['title_give']
+   print(title_receive)
+   return jsonify({'result':'success', 'msg': '이 요청은 POST!'})
+
+# $.ajax({
+#     type: "POST",
+#     url: "/test",
+#     data: { title_give:'봄날은간다' },
+#     success: function(response){
+#        console.log(response)
+#     }
+#   })
+
+if __name__ == '__main__':
+   app.run('0.0.0.0',port=5000,debug=True)
